7eecac568f08ff5f82ab841c9d42ac8e2b1c2fcc
S.R JENITH <93196792+srjenith@users.noreply.github.com>
2023-03-05 22:26:34 +0600

    Add files via upload

---
+++ portfolio_sd/js/main.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+
+    // typed js script
+    var typed = new Typed('.typed', {
+        // Waits 1000ms after typing "First"
+        strings: ['Web Designer...' , 'Web Developer...' , 'Graphic Designer...'],
+        showCursor : false,
+        loop : true,
+        typeSpeed : 100,
+    });
+
+
+
+
+
+
+
+
+
+
+})
